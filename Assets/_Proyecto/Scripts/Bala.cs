9d6b837e53d604b83a2c41767b4b63d3215938cb
AgustinV08 <32682024+AgustinV08@users.noreply.github.com>
2019-01-09 13:36:10 -0600

    update

---
+++ Assets/_Proyecto/Scripts/Bala.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour 
+{
+	
+	float TiempoEspera;
+
+	// Use this for initialization
+	void Start () 
+	{
+		TiempoEspera = 5.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		TiempoEspera -= Time.deltaTime;
+		
+		if(TiempoEspera <= 0.0f)
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
