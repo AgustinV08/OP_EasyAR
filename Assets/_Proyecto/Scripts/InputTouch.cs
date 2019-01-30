c64f011f59b759e815aab1a17ca003152ea4b5ed
agustin <agustin@alcatrazit.com>
2019-01-30 13:45:57 -0600

    Ya funcionaaaaaaaaa

--- Assets/_Proyecto/Scripts/InputTouch.cs
+++ Assets/_Proyecto/Scripts/InputTouch.cs
@@ -18,26 +18,25 @@ public class InputTouch : MonoBehaviour
         if(Input.touchCount > 0)
         {
             // El Input acaba de empezar
-            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                Debug.Log("Input Start");
+                //Debug.Log("Input Start");
 
                 Vector3 Posicion = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
                 Posicion.z = 0f;
                 GameObject go = Instantiate(Bala, Posicion, Quaternion.identity);
-                go.GetComponent<Rigidbody>().AddForce(1000 * transform.forward);
             }
 
-            // El input se esta moviendo por la pantalla
-            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+                // El input se esta moviendo por la pantalla
+                if (Input.GetTouch(0).phase == TouchPhase.Moved)
             {
-                Debug.Log("Input Moving");
+                //Debug.Log("Input Moving");
             }
 
             // El input se quedo tocando la pantalla sin moverse
             if(Input.GetTouch(0).phase == TouchPhase.Stationary)
             {
-                Debug.Log("Input Stationary");
+                //Debug.Log("Input Stationary");
 
                 Vector3 Posicion = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
                 Posicion.z = 0f;
@@ -48,7 +47,7 @@ public class InputTouch : MonoBehaviour
             // El input termino
             if(Input.GetTouch(0).phase == TouchPhase.Ended)
             {
-                Debug.Log("Input Ended");
+                //Debug.Log("Input Ended");
             }
         }
     }
