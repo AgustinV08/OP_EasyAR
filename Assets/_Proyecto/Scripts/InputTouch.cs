bb5c7288e92a810797857edee7c5b3eace7d238d
agustin <agustin@alcatrazit.com>
2019-02-06 10:27:21 -0600

    Touches implementados en 3D y 2D

--- Assets/_Proyecto/Scripts/InputTouch.cs
+++ Assets/_Proyecto/Scripts/InputTouch.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class InputTouch : MonoBehaviour
 {
+    // De esta manera solo funciona si la camara esta en 2D
+
     public GameObject Bala;
+
+    public Camera cam;
+
+    public LayerMask mascara;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +28,32 @@ public class InputTouch : MonoBehaviour
             {
                 //Debug.Log("Input Start");
 
-                Vector3 Posicion = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                // Camara 2D
+
+                /*Vector3 Posicion = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
                 Posicion.z = 0f;
-                GameObject go = Instantiate(Bala, Posicion, Quaternion.identity);
+                GameObject go = Instantiate(Bala, Posicion, Quaternion.identity);*/
+
+                // Camara 3D
+
+                RaycastHit hit;
+
+                Touch touch = Input.GetTouch(0);
+
+                Vector3 Posicion = cam.ScreenToWorldPoint(touch.position);
+
+                Ray rayo = cam.ScreenPointToRay(touch.position);
+
+                if (Physics.Raycast(rayo, out hit, Mathf.Infinity, mascara))
+                {
+                    Posicion.z = hit.transform.position.z;
+
+                    Instantiate(Bala, Posicion, Quaternion.identity);
+                }
             }
 
-                // El input se esta moviendo por la pantalla
-                if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            // El input se esta moviendo por la pantalla
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
             {
                 //Debug.Log("Input Moving");
             }
@@ -38,10 +63,10 @@ public class InputTouch : MonoBehaviour
             {
                 //Debug.Log("Input Stationary");
 
-                Vector3 Posicion = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                /*Vector3 Posicion = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
                 Posicion.z = 0f;
                 GameObject go = Instantiate(Bala, Posicion, Quaternion.identity);
-                go.GetComponent<Rigidbody>().AddForce(1000 * transform.forward);
+                go.GetComponent<Rigidbody>().AddForce(1000 * transform.forward);*/
             }
 
             // El input termino
