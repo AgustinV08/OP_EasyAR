cf710ec9f95a11ab0d798da8ad616e423ba89dde
agustin <villanuevaagustin77@gmail.com>
2019-02-19 13:38:42 -0600

    Reproducir Animaciones

---
+++ Assets/_Proyecto/Scripts/AnimacionesObjetos.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionesObjetos : MonoBehaviour
+{
+    // Encargado de las animaciones del objeto
+    public Animator anim;
+    
+    // Camera de donde se lanzara el Raycast
+    public Camera cam;
+    
+    // Mascata del objeto
+    public LayerMask mascara;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                // Objeto con el cual el Raycast colisionara
+                RaycastHit hit;
+
+                // Posicion del toque en la pantalla
+                Vector3 touchPos = Input.GetTouch(0).position;
+
+                // Linea invisible que se lanzara en la posicion donde tocamos la pantalla
+                Ray rayo = cam.ScreenPointToRay(touchPos);
+
+                // Si el rayo colisiona con el objeto con el layer indicado
+                if (Physics.Raycast(rayo, out hit, Mathf.Infinity, mascara))
+                {
+                    // Reproducimos la animacion del objeto
+                    anim.SetTrigger("ReproducirAnimacion");
+                }
+            }
+        }
+    }
+}
