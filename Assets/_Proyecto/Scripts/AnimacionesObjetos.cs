ca5b25c2e7414562a4c9ee43e78ccaf338d87e45
agustin <villanuevaagustin77@gmail.com>
2019-02-19 13:41:44 -0600

    Mas comentarios

--- Assets/_Proyecto/Scripts/AnimacionesObjetos.cs
+++ Assets/_Proyecto/Scripts/AnimacionesObjetos.cs
@@ -16,6 +16,7 @@ public class AnimacionesObjetos : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Igualamos la variable a la camara principal en escena
         cam = Camera.main;
     }
 
